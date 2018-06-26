903c5690585ec5f53c15bfb627f668a114a5bf12
Yash Goyal <32241989+goyalyash1224@users.noreply.github.com>
2018-06-26 15:15:03 +0530

    change "/" with "#"

--- knn.py
+++ knn.py
@@ -7,27 +7,27 @@ import seaborn as sns
 %matplotlib inline
 
 
-// Read the 'KNN_Project_Data csv file into a dataframe 
+# Read the 'KNN_Project_Data csv file into a dataframe 
 
 df = pd.read_csv('KNN_Project_Data')
 df.head() 
 
-//use seaborn on the dataframe to create a pairplot with the hue indicated by the TARGET CLASS column.
+#use seaborn on the dataframe to create a pairplot with the hue indicated by the TARGET CLASS column.
 
 sns.pairplot(df,hue='TARGET CLASS',palette='coolwarm')
 
 
-// now  standardize the variables.
+# now  standardize the variables.
 
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
-scaler.fit(df.drop('TARGET CLASS',axis=1))  //fit scaler to features
+scaler.fit(df.drop('TARGET CLASS',axis=1))  
 
-scaled_features = scaler.transform(df.drop('TARGET CLASS',axis=1)) //drop 'TARGET CLASS' from scaled features
+scaled_features = scaler.transform(df.drop('TARGET CLASS',axis=1)) 
 
 
-df_feat = pd.DataFrame(scaled_features,columns=df.columns[:-1]) //Convert the scaled features to a dataframe
+df_feat = pd.DataFrame(scaled_features,columns=df.columns[:-1]) 
 df_feat.head()
 
 # Train Test Split
@@ -38,11 +38,12 @@ X_train, X_test, y_train, y_test = train_test_split(scaled_features,df['TARGET C
                                                     test_size=0.30)
 
 
-from sklearn.neighbors import KNeighborsClassifier  //import kNeighborsClassifier 
+##use KNeighbors classifier to train data
 
+from sklearn.neighbors import KNeighborsClassifier  
 knn = KNeighborsClassifier(n_neighbors=1)
 
-knn.fit(X_train,y_train)  //fit the knn model for k=1
+knn.fit(X_train,y_train)  
 
 ##Evaluation of model
 
@@ -67,7 +68,7 @@ for i in range(1,40):
     pred_i = knn.predict(X_test)
     error_rate.append(np.mean(pred_i != y_test))
 
-//making a plot as error_rate vs k
+#making a plot as error_rate vs k
 plt.figure(figsize=(10,6))
 plt.plot(range(1,40),error_rate,color='blue', linestyle='dashed', marker='o',
          markerfacecolor='red', markersize=10)
